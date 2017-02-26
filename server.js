24cbb8a5fe9d05eb3d901253d145e7c10f557fe6
Michael Sholty <michael.sholty@sharpspring.com>
2017-02-26 09:10:40 -0500

    feat: use https

--- server.js
+++ server.js
@@ -3,24 +3,25 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    quiet: false,
-    noInfo: false,
-    stats: {
-        assets: false,
-        colors: true,
-        version: false,
-        hash: false,
-        timings: false,
-        chunks: false,
-        chunkModules: false
-    }
-}).listen(3000, 'localhost', function(err) {
-    if (err) {
-        console.log(err);
-    }
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  quiet: false,
+  noInfo: false,
+  https: true,
+  stats: {
+    assets: false,
+    colors: true,
+    version: false,
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false
+  }
+}).listen(3003, 'localhost', function(err) {
+  if (err) {
+    console.log(err);
+  }
 
-    console.log('Listening at localhost:3000');
+  console.log('Listening at https://localhost:3003');
 });
