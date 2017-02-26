5ddd684f856f1a9caf0d5eacdf224df7819b4463
Michael Sholty <michael.sholty@sharpspring.com>
2017-02-26 09:22:35 -0500

    feat: add list of rooms to lobby

--- app/Containers/Lobby/Lobby.jsx
+++ app/Containers/Lobby/Lobby.jsx
@@ -1,15 +1,67 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
-import { browserHistory } from 'react-router';
+import * as Colyseus from 'colyseus.js';
+import { browserHistory, Link } from 'react-router';
+import axios from 'axios';
 
-const Lobby = () => (
-  <div>
-    <h1>Lobby</h1>
-    <RaisedButton
-      label="Create new room"
-      onTouchTap={() => browserHistory.push('/draft/1234')}
-    />
-  </div>
-);
+class Lobby extends React.Component {
+  state = {
+    rooms: {},
+  };
+
+  componentDidMount() {
+    // fetch all rooms
+    axios('https://localhost:2657/rooms').then((response) => {
+      // eslint-disable-next-line
+      debugger;
+      this.setState({
+        rooms: response.data.rooms,
+      });
+    });
+    // this.getRooms();
+  }
+
+  getRooms = () => {
+    // eslint-disable-next-line
+    debugger;
+    this.setState({
+      rooms: this.client.rooms,
+    });
+  }
+
+  createDraftRoom = () => {
+    const config = {
+      id: this.client.id,
+    };
+    console.log(this.client.id);
+    axios.put('https://localhost:2657/rooms', config).then(() => {
+      browserHistory.push(`/draft/${config.id}`);
+    });
+  }
+
+  client = new Colyseus.Client('wss://localhost:2657');
+
+  render() {
+    const { rooms } = this.state;
+    console.log(rooms);
+
+    return (
+      <div>
+        <h1>Lobby</h1>
+        <RaisedButton
+          label="Create new room"
+          onTouchTap={this.createDraftRoom}
+        />
+        <ul>
+          {Object.keys(rooms).map(id => (
+            <li key={id}>
+              <Link to={rooms[id].roomName}>{rooms[id].roomName}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Lobby;
