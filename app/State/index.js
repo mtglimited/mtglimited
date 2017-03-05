5599a63410f083fda2a99512c1ec9502284f1b46
Michael Sholty <michael.sholty@sharpspring.com>
2017-03-05 14:31:47 -0500

    feat: add Drawer reducer/state

--- app/State/index.js
+++ app/State/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import configureStore from './CreateStore';
 import { reducer as sets } from './SetsRedux';
+import { reducer as drawer } from './DrawerRedux';
 
 export default () => configureStore(
   combineReducers({
     routing,
     sets,
+    drawer,
   }),
 );
