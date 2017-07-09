4675071807c99353c0c36ee10b22e87b8b683c64
Michael Sholty <michael.sholty@charterhook.com>
2017-07-08 20:19:47 -0400

    chore: add correct folder name

---
+++ app/Containers/Root/Root.prod.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import routes from 'routes';
+import createStore from 'State';
+
+const store = createStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+export default () => (
+  <Provider store={store}>
+    <Router history={history} routes={routes} />
+  </Provider>
+);
