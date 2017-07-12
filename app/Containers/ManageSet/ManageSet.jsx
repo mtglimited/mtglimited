330e69fd14a8583b0f3dc6541d78e0f764cc2696
Harry Gogonis <hgogonis@gmail.com>
2017-07-11 23:15:32 -0400

    refactor: update sets UI to put in Tiles

--- app/Containers/ManageSet/ManageSet.jsx
+++ app/Containers/ManageSet/ManageSet.jsx
@@ -4,6 +4,11 @@ import { fromJS } from 'immutable';
 import { firebaseConnect } from 'react-redux-firebase';
 import { connect } from 'react-redux';
 
+import Image from 'grommet/components/Image';
+import Box from 'grommet/components/Box';
+import Tiles from 'grommet/components/Tiles';
+import Tile from 'grommet/components/Tile';
+
 @firebaseConnect(({ params }) => [`/sets/${params.code}`])
 @connect(({ firebase }, { params }) => ({
   set: firebase.data.sets && fromJS(firebase.data.sets[params.code]),
@@ -22,19 +27,25 @@ export default class ManageSet extends React.Component {
     const code = set.get('code');
 
     return (
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Box>
         <h1>{set.get('name')}</h1>
-        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+
+        <Tiles
+          fill
+          flush={false}
+        >
           {set.get('cards').map(card => card && (
-            <img
-              alt={card.get('imageName')}
-              style={{ width: 223 }}
-              src={`${baseUrl}/${code}/${card.get('imageName')}.jpeg`}
-              key={card.imageName}
-            />
+            <Tile>
+              <Image
+                caption={card.get('imageName')}
+                size="small"
+                src={`${baseUrl}/${code}/${card.get('imageName')}.jpeg`}
+                key={card.imageName}
+              />
+            </Tile>
           ))}
-        </div>
-      </div>
+        </Tiles>
+      </Box>
     );
   }
 }
