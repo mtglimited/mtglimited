0f7b0e08b78a551d5f9df70bdab9966f659d8e78
Michael Sholty <michael.sholty@charterhook.com>
2017-07-12 17:09:17 -0400

    feat: add collection component

--- app/Containers/DraftLive/DraftLive.jsx
+++ app/Containers/DraftLive/DraftLive.jsx
@@ -5,6 +5,7 @@ import Immutable from 'immutable';
 import CircularProgress from 'material-ui/CircularProgress';
 import Seat from 'Containers/Seat';
 import axios from 'axios';
+import { isLoaded } from 'react-redux-firebase';
 
 const baseUrl = process.env.NODE_ENV === 'production'
   ? 'https://us-central1-mtglimited-154323.cloudfunctions.net/'
@@ -38,20 +39,17 @@ export default class DraftLive extends React.Component {
     const { seats, auth, sets, selectedSet, firebase, roomId } = this.props;
     const seatId = seats.findKey(s => s.get('owner') === auth.uid);
     const seat = seats.get(seatId);
-    if (!seat) {
+    if (!isLoaded()) {
       return <CircularProgress />;
     }
     const boosterQueue = seat.get('boosterQueue', new Immutable.Map());
     const packNumber = seat.get('packNumber');
-    const pickNumber = seat.get('pickNumber');
     const canOpenPack = boosterQueue.count() === 0 && seat.get('packNumber') < 3 && seat.get('pickNumber') % 15 === 0;
     const boosterSet = sets.getIn([selectedSet, 'draftOrder', packNumber - 1]);
 
     return (
       <div>
         <h1>Draft Live</h1>
-        <h2>Pack Number {packNumber}</h2>
-        <h2>Pick Number {pickNumber}</h2>
         { canOpenPack &&
           <RaisedButton
             label={`Open a ${boosterSet} booster pack`}
--- app/Containers/Seat/Seat.jsx
+++ app/Containers/Seat/Seat.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Immutable from 'immutable';
 import Booster from 'Containers/Booster';
+import Collection from 'Containers/Collection';
 
 const Seat = ({ sets, seat, seatId, boosterSet, pickCard }) => {
   if (!(seat && sets)) {
@@ -11,18 +12,34 @@ const Seat = ({ sets, seat, seatId, boosterSet, pickCard }) => {
   const set = sets.get(boosterSet);
   const boosterQueue = seat.get('boosterQueue', new Immutable.List());
   const boosterId = boosterQueue.first();
+  const collection = seat.get('collection');
+  const packNumber = seat.get('packNumber');
+  const pickNumber = seat.get('pickNumber');
 
   // TODO: display collection container
   return (
     <div>
-      {boosterId &&
-        <Booster
-          set={set}
-          seatId={seatId}
-          boosterId={boosterId}
-          pickCard={pickCard}
-        />
-      }
+      <h2>Current Pack</h2>
+      <h3>Pack Number {packNumber}, Pick Number {pickNumber}</h3>
+      <div style={{ overflowX: 'auto' }}>
+        {boosterId &&
+          <Booster
+            set={set}
+            seatId={seatId}
+            boosterId={boosterId}
+            pickCard={pickCard}
+          />
+        }
+      </div>
+      <h3>My Collection</h3>
+      <div style={{ overflowX: 'auto' }}>
+        {collection &&
+          <Collection
+            collectionId={collection}
+            sets={sets}
+          />
+        }
+      </div>
     </div>
   );
 };
